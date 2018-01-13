76f1b697dc2df1aed657245b89b7836f20aedd35
August <augustt198@gmail.com>
2018-01-13 12:46:37 -0500

    Add pin mapping

---
+++ src/bms/inc/pins.h
@@ -0,0 +1,32 @@
+#ifndef _PINS_H_
+#define _PINS_H_
+
+#define PIN_EEPROM_CS           2, 9
+#define PIN_GPIO1               1, 1
+#define PIN_GPIO2               1, 2
+#define PIN_CONTACTOR_WELD_1    1, 4
+#define PIN_CONTACTOR_WELD_2    1, 3
+#define PIN_LOW_SIDE_DETECT     1, 11
+#define PIN_HIGH_SIDE_DETECT    3, 2
+#define PIN_TEMP_CHECK          0, 3
+#define PIN_BMS_FAULT           3, 0
+#define PIN_CHARGER_ENABLE      3, 1
+#define PIN_LED1                1, 10
+#define PIN_LED2                2, 11
+#define PIN_LED3                1, 5
+
+#define PIN_IOCON_EEPROM_CS           IOCON_PIO2_9
+#define PIN_IOCON_GPIO1               IOCON_PIO1_1
+#define PIN_IOCON_GPIO2               IOCON_PIO1_2
+#define PIN_IOCON_CONTACTOR_WELD_1    IOCON_PIO1_4
+#define PIN_IOCON_CONTACTOR_WELD_2    IOCON_PIO1_3
+#define PIN_IOCON_LOW_SIDE_DETECT     IOCON_PIO1_11
+#define PIN_IOCON_HIGH_SIDE_DETECT    IOCON_PIO3_2
+#define PIN_IOCON_TEMP_CHECK          IOCON_PIO0_3
+#define PIN_IOCON_BMS_FAULT           IOCON_PIO3_0
+#define PIN_IOCON_CHARGER_ENABLE      IOCON_PIO3_1
+#define PIN_IOCON_LED1                IOCON_PIO1_10
+#define PIN_IOCON_LED2                IOCON_PIO2_11
+#define PIN_IOCON_LED3                IOCON_PIO1_5
+
+#endif
