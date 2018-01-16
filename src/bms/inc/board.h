bf0bb72176961c3f6f8935d6a40f86f62f6f2705
matthewha123 <root@DESKTOP-2TSVOSU.localdomain>
2018-01-16 15:43:53 -0500

    can_init

--- src/bms/inc/board.h
+++ src/bms/inc/board.h
@@ -1,8 +1,11 @@
+#include "../../../lib/lpc11cx4-library/lpc_chip_11cxx_lib/inc/chip.h"
+#include "can_raw.h"
+
 #ifndef _BOARD_H_
 #define _BOARD_H_
 
 #define Hertz2Ticks(freq) SystemCoreClock / freq
 void Board_Chip_Init(void);
 void Board_GPIO_Init(void);
-
+void Board_CAN_Init(uint32_t baudrate);
 #endif
---
+++ src/bms/inc/fsae_can.h
@@ -0,0 +1,9 @@
+#ifndef _FSAE_CAN_H
+#define _FSAE_CAN_H
+
+#include <stdint.h>
+
+
+void Can_Receive(void);
+
+#endif
