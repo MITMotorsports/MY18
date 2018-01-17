dd00c7490e1d070653369d42eee9029de0627665
matthewha123 <root@DESKTOP-2TSVOSU.localdomain>
2018-01-17 15:53:12 -0500

    can stuff

--- src/bms/inc/board.h
+++ src/bms/inc/board.h
@@ -7,5 +7,9 @@
 void Board_Chip_Init(void);
 void Board_GPIO_Init(void);
 void Board_CAN_Init(uint32_t baudrate);
-void Board_CAN_ProcessInput(/*BMS_INPUT,OUTPUT*/);
+void Board_CAN_ProcessInput(void/*BMS_INPUT,OUTPUT*/);
+void Board_LTC6804_ProcessInputs(void /*pack status*/);
+void Board_LTC6804_GetCellVoltages(void/*pack_status*/);
+void Board_LTC6804_GetCellTemperatures(void /*pack_status, bms_state->pack_config->num_modules*/);
+void Board_LTC6804_OpenWireTest(void);
 #endif
