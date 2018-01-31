168e0ff7e890feeeae163ee28d2d87926e5554e2
Nick Stathas <nistath@mit.edu>
2018-01-31 00:32:35 -0500

    Convert to new CANlib.

---
+++ src/bms/bin/discharge.d
@@ -0,0 +1,30 @@
+bin/discharge.o: src/discharge.c inc/discharge.h inc/state_types.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h inc/pack.h \
+ inc/state_types.h
+
+inc/discharge.h:
+
+inc/state_types.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+inc/pack.h:
+
+inc/state_types.h:
