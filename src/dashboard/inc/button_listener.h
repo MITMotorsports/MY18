be37089113d8a11f3f57532f7075208938d10a74
August <augustt198@gmail.com>
2018-02-19 02:34:48 -0500

    Add some comments

--- src/dashboard/inc/button_listener.h
+++ src/dashboard/inc/button_listener.h
@@ -3,12 +3,18 @@
 
 #include <stdbool.h>
 
+// tap: brief button press
+// hold: extended button press
+// none: nothing happened
 typedef enum {
     BUTTON_ACTION_TAP,
     BUTTON_ACTION_HOLD,
     BUTTON_ACTION_NONE
 } btn_action_t;
 
+// action field is only not BUTTON_ACTION_NONE
+// for a single update cycle... so check the value
+// every time you update.
 typedef struct {
     bool is_pressed;
     bool lockout;
@@ -17,6 +23,8 @@ typedef struct {
 } button_state_t;
 
 void init_button_state(button_state_t *state);
+
+// update the button state. `newval` == true means button pushed.
 void update_button_state(button_state_t *state, bool newval);
 
 #endif
