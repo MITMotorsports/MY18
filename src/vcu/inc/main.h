4c8a27957173ce1d1ab040604ec655c1878fad0a
Sebastian Lopez-Cot <slcot@mit.edu>
2018-02-20 22:27:15 -0500

    VCU updated canlib and can spec

--- src/vcu/inc/main.h
+++ src/vcu/inc/main.h
@@ -2,7 +2,7 @@
 #define __MAIN_H
 
 #include "stm32f2xx_hal.h"
-#include "Can_Library.h"
+#include "CANlib.h"
 
 #define CANx                            CAN1
 #define CANx_CLK_ENABLE()               __HAL_RCC_CAN1_CLK_ENABLE()
