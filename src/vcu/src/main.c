85997c55f398e10d6eb47261dee70e452ea7de4c
Sebastian Lopez-Cot <slcot@mit.edu>
2018-03-03 23:44:28 -0500

    Testing with usart

--- src/vcu/src/main.c
+++ src/vcu/src/main.c
@@ -2,6 +2,15 @@
 #include "main.h"
 
 #include <stdbool.h>
+#include "stdio.h"
+
+#ifdef __GNUC__
+  /* With GCC Compilers, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
 
 USART_HandleTypeDef  USARTHandle;
 CAN_HandleTypeDef    CanHandle;
@@ -55,6 +64,20 @@ int main(void)
   {
     loopVCU(&USARTHandle);
   } 
+}
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+  HAL_USART_Transmit(&USARTHandle, (uint8_t *)&ch, 1, 0xFFFF); 
+
+  return ch;
 }
 
 /**
--- src/vcu/src/vcu.c
+++ src/vcu/src/vcu.c
@@ -16,6 +16,14 @@ void loopVCU(USART_HandleTypeDef* uhandle) {
     update_implausibility(localBTState, &implaus_conflict_state);
     update_brake_throttle_conflict(localBTState, &implaus_conflict_state, board_heartbeats_state.frontCanNode);
 
+    printf("\r\n");
+    printf("\r\nhas_brake_throttle_conflict: %d\r\n", implaus_conflict_state.has_brake_throttle_conflict);
+    printf("\r\nobserved_implausibility: %d\r\n", implaus_conflict_state.observed_implausibility);
+    printf("\r\nactual_implausibility: %d\r\n", implaus_conflict_state.actual_implausibility);
+    printf("\r\nimplausibility_ticks: %d\r\n", implaus_conflict_state.implausibility_ticks);
+    printf("\r\n");
+    HAL_Delay(1000);
+
     // do some calc
     sendTorqueCmdMsg(10);
 }
