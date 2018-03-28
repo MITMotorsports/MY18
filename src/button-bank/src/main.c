39339aa361ef97901b825f163efdbfbf29285def
Nick Stathas <nickstathas@outlook.com>
2018-03-28 02:22:25 -0400

    Infinite love

--- src/button-bank/src/main.c
+++ src/button-bank/src/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 #define BTN_STRINGIFY(val) ((val) ? "pressed\n" : "released\n")
+#define OUT_STRINGIFY(val) ((val) ? "HIGH\n" : "LOW\n")
 #define SET_STRUCT_ZERO(name) memset(&name, 0, sizeof(name));
 
 volatile uint32_t msTicks;
@@ -8,20 +9,25 @@ volatile uint32_t msTicks;
 int main(void) {
   SystemCoreClockUpdate();
 
-  Serial_Init(UART_BAUD);
+  Board_UART_Init(UART_BAUD);
 
-  print("CAN INITIALIZATION\n");
+  Board_Print("CAN INITIALIZATION\n");
   CAN_Init();
 
   if (SysTick_Config(SystemCoreClock / 1000)) {
     while (1);  // error
   }
 
-  print("DONE INITIALIZING\n");
+  Board_Print("DONE INITIALIZING\n");
+
+  Board_Println("Currently running: "HASH);
+  Board_Println("Flashed by: "AUTHOR);
 
   button_states_t hold;
   SET_STRUCT_ZERO(hold);
   Frame p;
+
+  uint32_t last_buzz = 0;
 
   while (1) {
     // Clear RX buffer
@@ -33,6 +39,16 @@ int main(void) {
 
     if (send_buttonrequest(hold)) {
       SET_STRUCT_ZERO(hold);
+    }
+
+    if (false && msTicks - last_buzz > 1000) {
+      static bool buzz = false;
+      SET_PIN(BUZZER, buzz);
+      Board_Print("[BUZZER] ");
+      Board_Println(OUT_STRINGIFY(buzz));
+
+      buzz = !buzz;
+      last_buzz = msTicks;
     }
 
     print_buttons(current);
@@ -53,15 +69,13 @@ void print_buttons(button_states_t bs) {
   static button_states_t last_bs = {};
 
   if (bs.rtd != last_bs.rtd) {
-    print("RTD is ");
-    print(BTN_STRINGIFY(bs.rtd));
-    print("\n\n");
+    Board_Print("RTD is ");
+    Board_Println(BTN_STRINGIFY(bs.rtd));
   }
 
   if (bs.driver_reset != last_bs.driver_reset) {
-    print("DriverReset is ");
-    print(BTN_STRINGIFY(bs.driver_reset));
-    print("\n\n");
+    Board_Print("DriverReset is ");
+    Board_Println(BTN_STRINGIFY(bs.driver_reset));
   }
 
   last_bs = bs;
@@ -104,68 +118,68 @@ void handle_can_error(Can_ErrorID_T error) {
   if ((error != Can_Error_NONE) && (error != Can_Error_NO_RX)) {
     switch (error) {
     case Can_Error_NONE:
-      print("Can_Error_NONE\n");
+      Board_Print("Can_Error_NONE\n");
       break;
 
     case Can_Error_NO_RX:
-      print("Can_Error_NO_RX\n");
+      Board_Print("Can_Error_NO_RX\n");
       break;
 
     case Can_Error_EPASS:
-      print("Can_Error_EPASS\n");
+      Board_Print("Can_Error_EPASS\n");
       break;
 
     case Can_Error_WARN:
-      print("Can_Error_WARN\n");
+      Board_Print("Can_Error_WARN\n");
       break;
 
     case Can_Error_BOFF:
-      print("Can_Error_BOFF\n");
+      Board_Print("Can_Error_BOFF\n");
       break;
 
     case Can_Error_STUF:
-      print("Can_Error_STUF\n");
+      Board_Print("Can_Error_STUF\n");
       break;
 
     case Can_Error_FORM:
-      print("Can_Error_FORM\n");
+      Board_Print("Can_Error_FORM\n");
       break;
 
     case Can_Error_ACK:
-      print("Can_Error_ACK\n");
+      Board_Print("Can_Error_ACK\n");
       break;
 
     case Can_Error_BIT1:
-      print("Can_Error_BIT1\n");
+      Board_Print("Can_Error_BIT1\n");
       break;
 
     case Can_Error_BIT0:
-      print("Can_Error_BIT0\n");
+      Board_Print("Can_Error_BIT0\n");
       break;
 
     case Can_Error_CRC:
-      print("Can_Error_CRC\n");
+      Board_Print("Can_Error_CRC\n");
       break;
 
     case Can_Error_UNUSED:
-      print("Can_Error_UNUSED\n");
+      Board_Print("Can_Error_UNUSED\n");
       break;
 
     case Can_Error_UNRECOGNIZED_MSGOBJ:
-      print("Can_Error_UNRECOGNIZED_MSGOBJ\n");
+      Board_Print("Can_Error_UNRECOGNIZED_MSGOBJ\n");
       break;
 
     case Can_Error_UNRECOGNIZED_ERROR:
-      print("Can_Error_UNRECOGNIZED_ERROR\n");
+      Board_Print("Can_Error_UNRECOGNIZED_ERROR\n");
       break;
 
     case Can_Error_TX_BUFFER_FULL:
-      print("Can_Error_TX_BUFFER_FULL\n");
+      Board_Print("Can_Error_TX_BUFFER_FULL\n");
       CAN_Init();
       break;
 
     case Can_Error_RX_BUFFER_FULL:
-      print("Can_Error_RX_BUFFER_FULL\n");
+      Board_Print("Can_Error_RX_BUFFER_FULL\n");
       CAN_Init();
       break;
     }
@@ -173,32 +187,14 @@ void handle_can_error(Can_ErrorID_T error) {
 }
 
 // THE NETHER REGIONS -- TREAD CAREFULLY
-const uint32_t    OscRateIn = 12000000;
+const uint32_t OscRateIn = 12000000;
 
 void SysTick_Handler(void) {
   msTicks++;
 }
 
-void Serial_Init(uint32_t baudrate) {
-  Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_6, (IOCON_FUNC1 | IOCON_MODE_INACT));
-  Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));
-
-  Chip_UART_Init(LPC_USART);
-  Chip_UART_SetBaud(LPC_USART, baudrate);
-
-  // Configure data width, parity, and stop bits
-  Chip_UART_ConfigData(LPC_USART,
-                       (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT |
-                        UART_LCR_PARITY_DIS));
-  Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
-  Chip_UART_TXEnable(LPC_USART);
-}
-
-void print(const char *str) {
-  Chip_UART_SendBlocking(LPC_USART, str, strlen(str));
-}
-
 void GPIO_Init(void) {
+  /// INPUTS
   Chip_GPIO_SetPinDIRInput(LPC_GPIO, DRIVER_RST);
   Chip_IOCON_PinMuxSet(LPC_IOCON, DRIVER_RST_IOCON, BTN_CONFIG);
 
@@ -207,6 +203,10 @@ void GPIO_Init(void) {
 
   Chip_GPIO_SetPinDIRInput(LPC_GPIO, RTD);
   Chip_IOCON_PinMuxSet(LPC_IOCON, RTD_IOCON, BTN_CONFIG);
+
+  /// OUTPUTS
+  Chip_GPIO_SetPinDIROutput(LPC_GPIO, RTD);
+  Chip_IOCON_PinMuxSet(LPC_IOCON, RTD_IOCON, BUZZER_CONFIG);
 
   Chip_GPIO_Init(LPC_GPIO);
 }
