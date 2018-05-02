c29ca28a5d88ae6f65d376afb4def19dc2d8216f
Swissking1 <roderickbayliss@gmail.com>
2018-05-01 21:30:34 -0400

    Correct pin numbers

--- src/charger-shutdown-board/inc/pins.h
+++ src/charger-shutdown-board/inc/pins.h
@@ -1,15 +1,15 @@
 #ifndef _PINS_H_
 #define _PINS_H_
 
-#define PIN_BMS_FAULT           3, 0
-#define PIN_CHARGER_ENABLE      3, 1
-#define PIN_LED1                1, 10
-#define PIN_LED2                2, 11
+#define PIN_BMS_FAULT           2, 8
+#define PIN_CHARGER_ENABLE      1, 8
+#define PIN_LED1                2, 3
+#define PIN_LED2                1, 11
 
-#define PIN_IOCON_BMS_FAULT           IOCON_PIO3_0
-#define PIN_IOCON_CHARGER_ENABLE      IOCON_PIO3_1
-#define PIN_IOCON_LED1                IOCON_PIO1_10
-#define PIN_IOCON_LED2                IOCON_PIO2_11
+#define PIN_IOCON_BMS_FAULT           IOCON_PIO2_8
+#define PIN_IOCON_CHARGER_ENABLE      IOCON_PIO1_8
+#define PIN_IOCON_LED1                IOCON_PIO12_3
+#define PIN_IOCON_LED2                IOCON_PIO21_11
 
 #define IOCON_LED1_FUNC IOCON_FUNC0
 #define IOCON_LED2_FUNC IOCON_FUNC0
