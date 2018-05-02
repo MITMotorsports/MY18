69fd0346b9e96f99efebdefa5ad96be2e21997a0
Nick Stathas <nistath@mit.edu>
2018-05-01 21:09:36 -0400

    Resettable ESTOPs

--- src/vcu/src/state_error.c
+++ src/vcu/src/state_error.c
@@ -65,6 +65,18 @@ void enter_recoverable_error_state(void) {
 
 void update_recoverable_error_state(void) {
   TRANSITION_FATAL();
+
+  // Command both contactors open to avoid subsequent timeout faults
+  if (any_recoverable_gate_fault()) {
+    openLowSideContactor();
+    openHighSideContactor();
+
+    // Resume from LV state (precharge needs to happen again)
+    // This will let the car try to go through precharge again
+    // by flipping the DRIVER_RST pin.
+    set_vcu_state(VCU_STATE_LV);
+  }
+
   // If no recoverable faults appear it means we've cleared.
   if (!update_recoverable_faults()) set_error_state(NO_ERROR_STATE);
 
@@ -82,4 +94,20 @@ void update_fatal_error_state(void) {
 
 ERROR_STATE_T get_error_state(void) {
   return currentState;
+}
+
+bool error_may_advance(void) {
+  // If we're in a recoverable state and the only recoverable error is
+  // the shutdown gate then we can advance the VCU state, knowing that it is
+  // in VCU_STATE_LV due to update_recoverable_error_state.
+
+  (void) update_recoverable_faults();
+
+  bool recov_is_gate = get_error_state() == RECOVERABLE_ERROR_STATE &&
+                       recoverable_faults.gate &&
+                       !recoverable_faults.heartbeat &&
+                       !recoverable_faults.conflict &&
+                       !recoverable_faults.contactor;
+
+  return (get_error_state() == NO_ERROR_STATE) || recov_is_gate;
 }
