3c3c2dd54f2a23546dfd013d57b27cdac6c2e0d9
Nick Stathas <nistath@mit.edu>
2018-05-03 16:28:10 -0400

    Don't reenter forever

--- src/vcu/src/state_error.c
+++ src/vcu/src/state_error.c
@@ -74,7 +74,7 @@ void update_recoverable_error_state(void) {
     // Resume from LV state (precharge needs to happen again)
     // This will let the car try to go through precharge again
     // by flipping the DRIVER_RST pin.
-    set_vcu_state(VCU_STATE_LV);
+    if (get_vcu_state() != VCU_STATE_LV) set_vcu_state(VCU_STATE_LV);
   }
 
   // If no recoverable faults appear it means we've cleared.
