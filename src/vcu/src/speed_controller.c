b3966b28241c483afdea76125313b905835bd58d
Sebastian Lopez-Cot <slcot@mit.edu>
2018-10-13 14:21:04 -0400

    Precision updates should be correct now

--- src/vcu/src/speed_controller.c
+++ src/vcu/src/speed_controller.c
@@ -31,9 +31,9 @@ void init_speed_controller_defaults(int32_t max_input_speed,
     reset_internal_vars();
 
     // Parameters
-    speed_controller_params.kp_times_1000 = 1000;
+    speed_controller_params.kp_times_1000 = 200;
     speed_controller_params.ki_times_1000 = 0;
-    speed_controller_params.kd_times_1000 = 125;
+    speed_controller_params.kd_times_1000 = 0;
     speed_controller_params.i_windup_max = 10;
     speed_controller_params.i_windup_min = -10;
     speed_controller_params.min_output_value = 0;
@@ -63,8 +63,6 @@ void disable_controls(void) {
 }
 
 void set_speed_controller_setpoint(int32_t rpm) {
-    // TODO: Double-check dimensions of rpm and min/max input value
-
     // Input is too low
     if (rpm < speed_controller_params.min_input_value) {
         speed_controller_vars.out_of_input_range_throttled = true;
@@ -72,10 +70,8 @@ void set_speed_controller_setpoint(int32_t rpm) {
         return;
     }
 
-    // TODO: Double-check dimensions for these
     // Input is too high
     if (rpm > speed_controller_params.max_input_value) {
-
         speed_controller_vars.out_of_input_range_throttled = true;
         speed_controller_vars.rpm_setpoint = speed_controller_params.max_input_value;
         return;
@@ -105,6 +101,7 @@ void update_speed_controller_error(int32_t actual_rpm,
         return;
     }
 
+    // TODO: Overhead/underhead counter output so we know if we are not consistent w/ dt
     // Check to see if an update can be performed
     if (HAL_GetTick() - speed_controller_vars.last_error_update_timestamp 
         >= speed_controller_params.dt) {
@@ -122,14 +119,11 @@ static void update_error_internal(int32_t actual) {
     // take error diff
     speed_controller_vars.rpm_error = speed_controller_vars.rpm_setpoint - actual;
     
-    // TODO: Double-check units on division
-    // take derivative
+    // Multiply by 100 for precision, example: 1234 rpm * 1000 / 12ms = 102833, 1 rpm * 1000 / 12ms = 83    
     speed_controller_vars.deriv_rpm_error = 
         (speed_controller_vars.rpm_error 
-            - speed_controller_vars.last_rpm_error) / speed_controller_params.dt;
+            - speed_controller_vars.last_rpm_error) * 1000 / speed_controller_params.dt;
 
-    // TODO: Double-check dimensions for these
-    // take sum/integral
     int32_t accum = speed_controller_vars.rpm_error_accumulated 
                         + speed_controller_vars.rpm_error * speed_controller_params.dt;
 
@@ -149,12 +143,12 @@ int32_t get_speed_controller_torque_command(void) {
         return 0;
     }
 
-    // TODO: Double-check dimensions for these
-    int32_t torque = speed_controller_params.kp_times_1000 * speed_controller_vars.rpm_error
-        + speed_controller_params.ki_times_1000 * speed_controller_vars.rpm_error_accumulated
-        + speed_controller_params.kd_times_1000 * speed_controller_vars.deriv_rpm_error;
+    // CAREFUL! ADDING MORE PRECISION HERE WILL OVERFLOW THE INT32 FOR THE VALUES THE CONTROLLER IS EXPECTING
+    // RUN THIS ON SOME PAPER WITH HIGH AND LOW VALUES
+    int32_t torque = (speed_controller_params.kp_times_1000 * speed_controller_vars.rpm_error / 1000)
+        + (speed_controller_params.ki_times_1000 * speed_controller_vars.rpm_error_accumulated / 1000)
+        + (speed_controller_params.kd_times_1000 * speed_controller_vars.deriv_rpm_error / 1000);
 
-    // TODO: Double-check dimensions for these
     if (torque < speed_controller_params.min_output_value) {
         speed_controller_vars.out_of_output_range_throttled = true;
         return speed_controller_params.min_output_value;
@@ -170,39 +164,4 @@ int32_t get_speed_controller_torque_command(void) {
 
 bool get_speed_controller_enabled(void) {
   return enabled;
-}
-
-void send_speed_controller_info_can_msg(void) {
-    controller_info.out_of_input_range_throttled  = speed_controller_vars.out_of_input_range_throttled;
-    controller_info.out_of_output_range_throttled = speed_controller_vars.out_of_output_range_throttled;
-    controller_info.error_update_timed_out        = speed_controller_vars.error_update_timed_out;
-    controller_info.rpm_setpoint                  = speed_controller_vars.rpm_setpoint;
-    controller_info.commanded_torque              = speed_controller_vars.commanded_torque;
-    controller_info.rpm_error                     = speed_controller_vars.rpm_error;
-    controller_info.deriv_rpm_error               = speed_controller_vars.deriv_rpm_error;
-    controller_info.time_since_last_error_update  = speed_controller_vars.time_since_last_error_update;
-    controller_info.last_rpm_error                = speed_controller_vars.last_rpm_error;
-    controller_info.rpm_error_accumulated         = speed_controller_vars.rpm_error_accumulated;
-
-    // TODO: send can msg
-    
-    // Set any true values to false since we have recorded the event, and any false values remain the same
-    speed_controller_vars.out_of_input_range_throttled = false;
-    speed_controller_vars.out_of_output_range_throttled = false;
-}
-
-void send_speed_controller_params_can_msg(void) {
-    controller_params.kp_times_1000        = speed_controller_params.kp_times_1000;
-    controller_params.ki_times_1000        = speed_controller_params.ki_times_1000;
-    controller_params.kd_times_1000        = speed_controller_params.kd_times_1000;
-    controller_params.i_windup_max         = speed_controller_params.i_windup_max;
-    controller_params.i_windup_min         = speed_controller_params.i_windup_min;
-    controller_params.min_output_value     = speed_controller_params.min_output_value;
-    controller_params.max_output_value     = speed_controller_params.max_output_value;
-    controller_params.min_input_value      = speed_controller_params.min_input_value;
-    controller_params.max_input_value      = speed_controller_params.max_input_value;
-    controller_params.error_update_timeout = speed_controller_params.error_update_timeout;
-    controller_params.dt                   = speed_controller_params.dt;
-
-    // TODO: send can msg
 }
