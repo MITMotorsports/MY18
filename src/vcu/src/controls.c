39507f475767e8b9e277a47a4ced7a6cbd2a7573
ajmel <alvaromelendezh@gmail.com>
2019-02-09 15:17:11 -0500

    Fixed ramp code counter
    Fixed power limiting torque command ramp counter

--- src/vcu/src/controls.c
+++ src/vcu/src/controls.c
@@ -167,15 +167,15 @@ static int32_t get_regen_torque() {
 
 static int32_t get_power_limited_torque(int32_t pedal_torque) {
   static int32_t tCAP = 0;
-  static int32_t count = 1;
+  static int32_t count = 0;
   
   if(speed < 0) { // check car is moving forward
     
-    if(count <= 5) { // ramp is not over yet
+    if(count < 5 && count != 0) { // ramp is not over yet
 
       if(pedal_torque >= tMAX) { // check if car pedal is above allowed torque
         count++; // increase counter
-        return tCAP*count*100/600; // return tCAP in incrementing fifths
+        return tCAP*count*100/500; // return tCAP in incrementing fifths
       }
       count = 0; // else, reset the counter and return pedal
       tCAP = 0;
@@ -185,7 +185,7 @@ static int32_t get_power_limited_torque(int32_t pedal_torque) {
       int32_t tMAX = power_limit*6000*10/((0-mc_readings.speed)*628); // calculate tMAX based on power limit and wheel speed
       if(tMAX > MAX_TORQUE) tMAX = MAX_TORQUE; // cap at max torque
       tCAP = tMAX;
-      
+      count++;
       return tCAP/5;
     }
     
