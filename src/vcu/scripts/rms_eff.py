3fd9736c7faa4732abb2d2d2d8eec7e2c1ddcf82
Dani White <white.dani.m@gmail.com>
2019-03-30 18:55:45 -0400

    Add code which is too slow for guessing eff

--- src/vcu/scripts/rms_eff.py
+++ src/vcu/scripts/rms_eff.py
@@ -18,7 +18,8 @@ full_path = base.joinpath(str(date), "numpy", str(time2) + ".npz")
 filepaths = [
     (20180615, 191103),
     (20180615, 193838),
-    (20180615, 194829)
+    (20180615, 194829),
+    # (20190330, "040403")
 ]
 
 def shift(arr, n=1):
@@ -79,6 +80,34 @@ for date, time in filepaths:
     trqs = np.append(trqs, trq_at_iq_fb_times/10)
     effs = np.append(effs, eff)
 
-plt.scatter(spds, trqs, c=effs, cmap='plasma', s=200)
+# print("Max speed:", np.max(spds))
+
+# plt.subplot(211)
+plt.scatter(spds, trqs, c=effs, cmap='plasma', s=20)
 plt.colorbar()
 plt.show()
+
+# eff_interp_fin = interpolate.interp2d(spds, trqs, effs)
+# _new_spds = np.linspace(0, 6000, 10)
+# _new_trqs = np.linspace(0, 240, 10)
+# new_spds, new_trqs = np.meshgrid(_new_spds, _new_trqs)
+# new_eff = eff_interp_fin(new_spds, new_trqs)
+# plt.scatter(new_spds, new_trqs, c=new_eff[0,:], cmap='plasma', s=200)
+# grid = interpolate.griddata((spds, trqs), effs, (_new_spds, _new_trqs), method='nearest')
+
+# grid_x, grid_y = np.mgrid[0:6000:60j, 0:240:20j]
+# grid = interpolate.griddata((spds, trqs), effs, (grid_x, grid_y), method='nearest')
+
+# plt.subplot(212)
+# plt.scatter(grid_x, grid_y, c=grid, cmap='plasma', s=20)
+# plt.colorbar()
+# plt.show()
+
+# numpy.savez("grid.npz", grid_x=grid_x, grid_y=grid_y, grid=grid)
+
+# for row in grid:
+#     print "  {",
+#     for val in enumerate(row):
+#         print "{},".format(int(val * 1000))
+#     print "},"
+# print "}"
